38ad49b7d5911d2c927853a491dff30af069d9be
Neerja <neerajdayalan@gmail.com>
2022-11-26 16:35:42 +0530

    :constructor: Bug fixed"

--- student_management/main.py
+++ student_management/main.py
@@ -3,5 +3,5 @@ from student_management.utils.view import View
 # A special condition that gets executed only when you run the Python file from the command line.
 if __name__ == "__main__":
     # Calling the `display_welcome_sms` and `get_application_inputs` methods of the `View` class.
-    View.display_welcome_sms()
+    print(View.display_welcome_sms())
     View.get_application_inputs()
--- student_management/model/settings.py
+++ student_management/model/settings.py
@@ -9,24 +9,26 @@ are the paths to the sample role and
 class Settings(BaseSettings):
     role_sample_path = r"C:\Users\Welcome\python projects\student_management\data\sample_role.json"
     user_sample_path = r"C:\Users\Welcome\python projects\student_management\data\sample_users.json"
-    select_role_option_text = f"""Please select the below option to manage roles:\n
-                    1. Add Role\n
-                    2. Delete Role\n
-                    3. Update Role\n
-                    4. Search Role\n
-                    5. Fetch Role\n
-                    6. Exit"""
+    select_role_option_text = f"""Please select the below option to manage roles:
+1. Add Role
+2. Delete Role
+3. Update Role
+4. Search Role
+5. Fetch Role
+6. Exit
+"""
     welcome_sms_text = f"""
-                f"{4 * '*'} Student Management System {4 * '*'}"
-                    Please select the below option:  \n
-                    1. Manage Role \n
-                    2. Manage Users \n
-                    3. Exit \n
+{4 * '*'} Student Management System {4 * '*'}
+Please select the below option:  
+1. Manage Role 
+2. Manage Users 
+3. Exit 
                 """
-    select_user_option_text = """Please select the below option to manage users:\n
-                    1. Add User\n
-                    2. Delete User\n
-                    3. Update User\n
-                    4. Search User\n
-                    5. Fetch User\n
-                    6. Exit"""
+    select_user_option_text = """Please select the below option to manage users:
+1. Add User
+2. Delete User
+3. Update User
+4. Search User
+5. Fetch User
+6. Exit
+                    """
